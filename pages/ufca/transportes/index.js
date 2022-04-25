c7d9bb30f62f640849044b96d0dbcef54f5640a5
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2022-04-24 23:28:14 -0300

    Update index.js

--- pages/ufca/transportes/index.js
+++ pages/ufca/transportes/index.js
@@ -1,22 +1,21 @@
 import { useState } from 'react'
 
 export default function Transportes() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([])
   const getData = async () => {
-    const response = await fetch('/api/ufca/transportes/verifica');
-    const values = await response.json();
-    setData(values);
+    const response = await fetch('/api/ufca/transportes/verifica')
+    const values = await response.json()
+    setData(values)
   }
 
   return (
     <div>
       <h2>Transportes</h2>
+
       <div>
         <button onClick={getData}>Carregar dados</button>
         <ul>
-          {data.map(item => (
-            <li>{item.responsavel}</li>
-          ))}
+          {data.map((item) => <li key={item.responsavel}>{item.responsavel}</li>)}
         </ul>
       </div>
 
