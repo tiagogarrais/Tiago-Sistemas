482c7e03d565f4f598d389491ecb1eb406bd16b7
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2022-04-25 00:19:24 -0300

    Pequenas atualizações

--- pages/api/ufca/transportes/verifica.js
+++ pages/api/ufca/transportes/verifica.js
@@ -1,9 +1,14 @@
 export default function verifica (req, res){
   if (req.method === 'GET'){
     res.status(200).json([
-      { responsavel: 'Fulano' },
-      { responsavel: 'Cicrano' },
-      { responsavel: 'Beltrano' },
+      { responsavel: 'Mikasa' },
+      { responsavel: 'Ayaka' },
+      { responsavel: 'Beidou' },
+      { responsavel: 'Xiyan' },
+      { responsavel: 'Bennet' },
+      { responsavel: 'Eren' },
+      { responsavel: 'Armin' },
+
     ])
   } else{
     res.status(405).end('Não permitido')
--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,19 @@ export default function Home() {
     <div>
       <Head>
         <title>Tiago Sistemas</title>
-        <meta name="description" content="Sistemas informatizados desenvolvidos por Tiago" />
+        <meta
+          name="description"
+          content="Sistemas informatizados desenvolvidos por Tiago"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1>Tiago Sistemas</h1>
-          <h2>Organizações</h2>
-        <a href="./ufca"><button>UFCA</button></a>
+        <h2>Organizações</h2>
+        <a href="./ufca">
+          <button>UFCA</button>
+        </a>
       </main>
     </div>
   )
